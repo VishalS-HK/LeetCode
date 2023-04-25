7524e41e3a499f7ac12732e1b9e33395749aac8f
Vishal S <99802796+VishalS-HK@users.noreply.github.com>
2023-04-25 10:34:33 +0530

    Time: 0 ms (100.00%), Space: 39.5 MB (68.45%) - LeetHub

---
+++ 0461-hamming-distance/0461-hamming-distance.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int hammingDistance(int x, int y) {
+        int HammingDistance = 0;
+        for(int k = x ^ y; k !=0; k = k & (k - 1))
+            HammingDistance++;
+        return HammingDistance;
+    }
+}
